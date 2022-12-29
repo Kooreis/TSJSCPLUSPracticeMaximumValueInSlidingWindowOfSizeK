4ab7fd1cb34da7c581a79f10edf5c772a6a403c9
Kooreis <kooreis.official@gmail.com>
2022-12-28 21:42:58 -0600

    Added three methods to the 'SlidingWindowMax' class: 'push', 'max', and 'pop'. 'push' adds a value to the deque, removing all smaller values from the end. 'max' returns the maximum value from the deque. 'pop' removes a value from the deque if it is the maximum.

--- solution.ts
+++ solution.ts
@@ -1,3 +1,16 @@
-class SlidingWindowMax {
-    private deque: number[] = [];
-}
+push(val: number): void {
+        while (this.deque.length && this.deque[this.deque.length - 1] < val) {
+            this.deque.pop();
+        }
+        this.deque.push(val);
+    }
+
+    max(): number {
+        return this.deque[0];
+    }
+
+    pop(val: number): void {
+        if (this.deque[0] === val) {
+            this.deque.shift();
+        }
+    }
