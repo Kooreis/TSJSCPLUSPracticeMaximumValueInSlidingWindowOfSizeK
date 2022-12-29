e9ef05ca21e1f840ec9548acf3f5d37ff63db7d8
Kooreis <kooreis.official@gmail.com>
2022-12-28 21:42:58 -0600

    This update introduces the maxSlidingWindow function. It starts by checking if the input array is empty or if the window size is larger than the array. Then it initializes an empty result array and a new Deque. It then fills the Deque with the indices of the first k elements of the array, removing any elements from the rear of the Deque that are smaller than the current element.

--- solution.cs
+++ solution.cs
@@ -1,45 +1,15 @@
-class Deque {
-    constructor() {
-        this.items = [];
-    }
+function maxSlidingWindow(nums, k) {
+    let n = nums.length;
+    if (n == 0) return [];
+    if (k > n) return [];
 
-    addFront(element) {
-        this.items.unshift(element);
-    }
+    let res = [];
+    let dq = new Deque();
 
-    addRear(element) {
-        this.items.push(element);
-    }
-
-    removeFront() {
-        if(this.isEmpty())
-            return "Underflow";
-        return this.items.shift();
-    }
-
-    removeRear() {
-        if(this.isEmpty())
-            return "Underflow";
-        return this.items.pop();
-    }
-
-    front() {
-        if(this.isEmpty())
-            return "No elements in Deque";
-        return this.items[0];
-    }
-
-    rear() {
-        if(this.isEmpty())
-            return "No elements in Deque";
-        return this.items[this.items.length - 1];
-    }
-
-    isEmpty() {
-        return this.items.length == 0;
-    }
-
-    size() {
-        return this.items.length;
+    for (let i = 0; i < k; i++) {
+        while (!dq.isEmpty() && nums[i] >= nums[dq.rear()]) {
+            dq.removeRear();
+        }
+        dq.addRear(i);
     }
 }
